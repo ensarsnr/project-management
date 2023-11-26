bcc241aaeb535e89bd07cbd6455d509a50c2078d
ensarsnr <dev.anise@outlook.com>
2023-11-26 23:22:10 +0300

    Added mongodb connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const app = express()
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+const app = express();
 const port = process.env.PORT || 3001;
 
-app.listen(port, ()=> {
-console.log("Connected server");
-})
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/project-management", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error: ", error);
+  });
+
+app.listen(port, () => {
+  console.log(`Connected server ${port}`);
+});
