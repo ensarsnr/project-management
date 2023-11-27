3479ba3a5fd3c4dde15334458971779002e65da9
ensarsnr <dev.anise@outlook.com>
2023-11-27 15:14:30 +0300

    Created auth controller

---
+++ backend/src/controllers/userAuth.js
@@ -0,0 +1,40 @@
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt');
+// güvenlik için önemli!
+
+const User = require("../models/user");
+
+// Yeni bir kullanıcı kaydet
+const register = async (req, res) => {
+  const { name, surname, username, password } = req.body;
+
+  try {
+    const user = new User({ name, surname, username, password });
+    await user.save();
+    res.json({ message: "Registration successful" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Mevcut bir kullanıcı ile giriş yapın
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Mevcut kullanıcıyı bulmak için kullanılıyor
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    // comparePassword giriş yapmaya çalışan kullanıcının şifresini db de karşılaştırıyor.
+    const password = await User.comparePassword({ password });
+    if (!password) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { register, login };
