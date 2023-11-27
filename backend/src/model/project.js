0a15a8dae43b522cde51c627ba8c904bc1353ef4
ensarsnr <dev.anise@outlook.com>
2023-11-27 15:18:10 +0300

    Created user routes file

--- backend/src/model/project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-});
-
-module.exports = mongoose.model("Project", ProjectSchema);
