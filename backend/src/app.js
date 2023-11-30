25d5e40ce616f63a6d25dfefa55d8e7614569092
ensarsnr <dev.anise@outlook.com>
2023-11-30 17:16:24 +0300

    Updated file and added dotenv module

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db");
+const dotenv = require("dotenv");
+
+//routes
 const authRoutes = require("./routes/userAuth");
+const projectRoutes = require("./routes/project");
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,7 +17,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Endpoinler
 app.use("/auth", authRoutes);
+app.use("/project", projectRoutes);
 
 app.listen(port, () => {
   console.log(`Connected server ${port}`);
