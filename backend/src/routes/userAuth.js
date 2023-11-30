033ceb8502c488cc7ae5a40f15f72904b3f23a2a
ensarsnr <dev.anise@outlook.com>
2023-11-30 17:18:45 +0300

    Fixed bugs

--- backend/src/routes/userAuth.js
+++ backend/src/routes/userAuth.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const { register, login } = require("../controllers/userAuth");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
+// Kullanıcı kaydı
 router.post("/register", register);
+
+// Kullanıcı girişi
 router.post("/login", login);
+
+// Token doğrulama middleware'i ile korunan bir route
+router.get("/protected", authMiddleware, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.user });
+});
 
 module.exports = router;
