77347556c94faf527014bbaeaccd4d5f20eba4c8
ensarsnr <dev.anise@outlook.com>
2023-12-11 20:26:17 +0300

    Register process has been completed

--- frontend/src/pages/auth/Register.vue
+++ frontend/src/pages/auth/Register.vue
@@ -7,29 +7,37 @@
             <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                 <div class="max-w-md mx-auto">
                     <div>
-                        <h1 class="text-2xl font-semibold">Login Form with Floating Labels</h1>
+                        <h1 class="text-4xl text-center font-semibold">Create Account
+                        </h1>
                     </div>
                     <div class="divide-y divide-gray-200">
                         <form @submit="handleSubmit"
                             class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                            <div class="relative">
-                                <input :value="username" @input="onchangeUsername" autocomplete="off" id="username"
-                                    name="username" type="text"
-                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                                    placeholder="Username" />
-                                <label for="email"
-                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                            <div class="relative h-11 w-full min-w-[200px]">
+                                <input :value="username" @input="onchangeUsername"
+                                    class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                                    placeholder=" " />
+                                <label
+                                    class="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                    Username
+                                </label>
                             </div>
-                            <div class="relative">
-                                <input :value="password" @input="password" autocomplete="off" id="password" name="password"
-                                    type="password"
-                                    class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                                    placeholder="Password" />
-                                <label for="password"
-                                    class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                            <div class="relative h-11 w-full min-w-[200px]">
+                                <input :value="password" @input="onchangePassword" type="password"
+                                    class="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                                    placeholder=" " />
+                                <label
+                                    class="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                                    Password
+                                </label>
                             </div>
-                            <div class="relative">
-                                <button class="bg-blue-500 text-white rounded-md px-2 py-1">Register</button>
+                            <div class="text-center">
+                                <button class="w-3/4 bg-blue-500 text-white rounded-md px-2 py-1">Register</button>
+                            </div>
+                            <div class="text-center">
+                                <router-link class="text-sm text-teal-500 hover:text-teal-600" tag="a" to="/">Don't
+                                    you have an
+                                    account?</router-link>
                             </div>
                         </form>
                     </div>
@@ -42,6 +50,8 @@
 
 
 <script>
+import services from '../../services';
+
 export default {
     data() {
         return {
@@ -58,6 +68,18 @@ export default {
         },
         handleSubmit(e) {
             e.preventDefault();
+            this.registerUser();
+        },
+        async registerUser() {
+            try {
+                const response = services.register(
+                    this.username,
+                    this.password
+                );
+                console.log("Register Response: ", response)
+            } catch (error) {
+                console.log(error);
+            }
         }
 
     },
