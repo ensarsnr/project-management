05d611c18f9cf3fa1ea999c3b25ac379c454de58
ensarsnr <dev.anise@outlook.com>
2023-12-12 22:32:17 +0300

    Created Navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start">
+                    <h1 class="text-white text-2xl">Project Management</h1>
+                </div>
+                <div class="">
+                    <ul class="flex justify-between">
+                        <li>
+                            <button @click="signOut"
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                Sign Out
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+  
+<script>
+export default {
+    methods: {
+        signOut() {
+            localStorage.clear();
+            this.$router.push("/");
+        },
+    },
+};
+</script>
+  
