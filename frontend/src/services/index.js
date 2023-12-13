d02e6f1bdfa4d29dfe4294b816cf094a0bee213b
ensarsnr <dev.anise@outlook.com>
2023-12-13 22:06:07 +0300

    Axios services added, getProjects

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,8 @@
 import login from "./auth/login";
 import register from "./auth/register";
 import createProject from "./project/createProject";
+import getProjects from "./project/getProjects";
 
-const services = { login, register, createProject };
+const services = { login, register, createProject, getProjects };
 
 export default services;
