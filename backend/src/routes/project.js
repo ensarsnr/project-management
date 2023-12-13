f1849c721d50bac557d6dbcea8fe6f31d71306ca
ensarsnr <dev.anise@outlook.com>
2023-12-13 15:26:06 +0300

    Added new router get-projects

--- backend/src/routes/project.js
+++ backend/src/routes/project.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { createProject, addUser } = require("../controllers/project");
+const {
+  createProject,
+  addUser,
+  getProjects,
+} = require("../controllers/project");
 const authMiddleware = require("../middleware/auth.middleware");
 
 const router = express.Router();
@@ -7,5 +11,6 @@ const router = express.Router();
 // Giriş yapmış kullanıcıya özel işlemler
 router.post("/create-project", authMiddleware, createProject);
 router.post("/add-user", addUser);
+router.get("/get-projects", authMiddleware, getProjects);
 
 module.exports = router;
