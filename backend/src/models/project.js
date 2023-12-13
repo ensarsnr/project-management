cd139bc95e529e7ecda73b4227fa9def4c0845a1
ensarsnr <dev.anise@outlook.com>
2023-12-13 15:49:06 +0300

    Desc added to project schema

--- backend/src/models/project.js
+++ backend/src/models/project.js
@@ -4,6 +4,7 @@ const User = require("./user");
 
 const ProjectSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  desc: { type: String, required: true },
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
