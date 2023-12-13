737e885bf241d3eac23c9ed0947eeaa5378380ff
ensarsnr <dev.anise@outlook.com>
2023-12-13 16:10:40 +0300

    Added Authorization header to API requests

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,12 @@
+// api.js
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3001",
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
