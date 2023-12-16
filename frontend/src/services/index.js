299ce62f516da4ae5f229d5f54bb7e3d4a8bef1a
ensarsnr <dev.anise@outlook.com>
2023-12-16 23:07:02 +0300

    New services added

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,7 +2,8 @@ import login from "./auth/login";
 import register from "./auth/register";
 import createProject from "./project/createProject";
 import getProjects from "./project/getProjects";
+import deleteProject from "./project/deleteProject";
 
-const services = { login, register, createProject, getProjects };
+const services = { login, register, createProject, getProjects, deleteProject };
 
 export default services;
